1fab4a3b15c7e8574a76d5850ec67c1be95d6f3f
gaultier.large <gaultier.large@diiage.org>
2024-01-12 16:23:50 +0100

    Add Dependency injection

--- Program.cs
+++ Program.cs
@@ -6,28 +6,40 @@ using CRM.Domain;
 using CRM.Infrasctructure.Persistence;
 using CRM.Infrasctructure.Persistence.repositories;
 using CRM.Shared.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using System.Text;
 
 Console.WriteLine("WelCome To my CRM");
 
+ServiceCollection services = new ServiceCollection();
+services.AddTransient<IPersistenceService, PersistenceService>();
+services.AddTransient<IRepository<Customer>, CustomerRepository>();
+services.AddTransient<CreateCustomerCommand>();
+services.AddTransient<GetCustomerNumberQuery>();
+
+
+var Container = services.BuildServiceProvider();
 
 
 
-IPersistenceService persistenceService = new PersistenceService(new CustomerRepository());
-CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(persistenceService);
+IPersistenceService? persistenceService = Container.GetService<IPersistenceService>();
+CreateCustomerCommand? createCustomerCommand = Container.GetService<CreateCustomerCommand>();
 
+if(createCustomerCommand != null)
+{ 
 createCustomerCommand.Execute(new CRM.Application.Customer.CustomerDTO()
 {
     CompanyName = "Mon Client",
     Type = CRM.Domain.Enum.EnumCustomerType.Customer
-
 }) ;
+}
 
 
+GetCustomerNumberQuery? getCustomerNumberQuery = Container.GetService<GetCustomerNumberQuery>();
+if (getCustomerNumberQuery != null)
+{
+    var nbCustomer = getCustomerNumberQuery.Execute();
 
-GetCustomerNumberQuery getCustomerNumberQuery = new GetCustomerNumberQuery(persistenceService);
-var nbCustomer = getCustomerNumberQuery.Execute();
-
-Console.WriteLine($"Le nombre de clienst est de {nbCustomer}");
-
+    Console.WriteLine($"Le nombre de clienst est de {nbCustomer}");
+}
 
